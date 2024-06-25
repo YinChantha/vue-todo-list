2c45a60a584d8f370c93d2a894eb2a7cc0fa28fe
Chantha YIN <chantha.yin@goodhill.com.kh>
2024-06-25 11:16:16 +0700

    finalize

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { Icon } from '@iconify/vue'
-import VueWriter from "vue-writer";
 
 const app = createApp(App);
 app.component('Icon', Icon)
